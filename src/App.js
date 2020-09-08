b1fab1b93e39658d857782be5613e49bceafacf2
kharchenkok <k.v.kharchenko@ukr.net>
2020-09-08 17:06:55 +0300

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+import News from './components/news/News';
+// import ContactForm from './components/contactForm/ContactForm';
+
+
+// id: uuidv4() 
+
+export default function App() {
+  return <>
+  <News />
+  </>
+  
+}
---
+++ src/components/news/News.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import Styles from './ContactFormStyles.module.css';
+
+export default class News extends Component {
+  // state = {
+  //   name: '',
+  //   number: '',
+  // };
+  render(){
+return <>
+<h2>News</h2>
+</>
+  }
+}
+
+// ContactForm.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       name:PropTypes.string,
+//       number:PropTypes.string,
+//       id:PropTypes.string
+
+//     })
+//   ).isRequired,
+//   addUserContact: PropTypes.func.isRequired,
+// };
+
+
